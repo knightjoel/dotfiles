5b3af3cf84990ac16ed96a6a56e84feed0c8503b
Joel Knight <knight.joel@gmail.com>
2023-07-03 14:15:01 -0600

    fix(nvim): reduce usage of alt key
    Prefer other 'leader' keys in key mappings to reduce likelihood of a
    collision with the app/OS hotkeys.

--- .config/nvim/lua/user/core/keymaps.lua
+++ .config/nvim/lua/user/core/keymaps.lua
@@ -37,8 +37,6 @@ M.setup = {
     -- turn off highlighted search results (in all modes)
     map("", "<Esc>", ":nohl<CR>")
     map("n", "<Leader>e", ":NvimTreeToggle<CR>")
-    -- quit
-    map("n", "<a-q>", ":quit<CR>")
 
     -- Better indentation/deindentation of code blocks
     map("v", "<", "<gv")
@@ -55,9 +53,6 @@ M.setup = {
     map("v", "<Tab>", ">gv")
     map("v", "<S-tab>", "<gv")
 
-    -- close buffer using barbar plugin
-    map("n", "<a-w>", ":BufferClose<CR>")
-
     -- delete single character without copying into register
     map("n", "x", '"_x')
 
@@ -68,24 +63,26 @@ M.setup = {
 
   barbar = function()
     -- Move to previous/next
-    map("n", "<A-,>", "<Cmd>BufferPrevious<CR>")
-    map("n", "<A-.>", "<Cmd>BufferNext<CR>")
+    map("n", "<Leader>j", "<Cmd>BufferPrevious<CR>")
+    map("n", "<Leader>k", "<Cmd>BufferNext<CR>")
     -- Re-order to previous/next
     map("n", "<A-<>", "<Cmd>BufferMovePrevious<CR>")
     map("n", "<A->>", "<Cmd>BufferMoveNext<CR>")
     -- Goto buffer in position...
-    map("n", "<A-1>", "<Cmd>BufferGoto 1<CR>")
-    map("n", "<A-2>", "<Cmd>BufferGoto 2<CR>")
-    map("n", "<A-3>", "<Cmd>BufferGoto 3<CR>")
-    map("n", "<A-4>", "<Cmd>BufferGoto 4<CR>")
-    map("n", "<A-5>", "<Cmd>BufferGoto 5<CR>")
-    map("n", "<A-6>", "<Cmd>BufferGoto 6<CR>")
-    map("n", "<A-7>", "<Cmd>BufferGoto 7<CR>")
-    map("n", "<A-8>", "<Cmd>BufferGoto 8<CR>")
-    map("n", "<A-9>", "<Cmd>BufferGoto 9<CR>")
-    map("n", "<A-0>", "<Cmd>BufferLast<CR>")
+    map("n", "<Leader>1", "<Cmd>BufferGoto 1<CR>")
+    map("n", "<Leader>2", "<Cmd>BufferGoto 2<CR>")
+    map("n", "<Leader>3", "<Cmd>BufferGoto 3<CR>")
+    map("n", "<Leader>4", "<Cmd>BufferGoto 4<CR>")
+    map("n", "<Leader>5", "<Cmd>BufferGoto 5<CR>")
+    map("n", "<Leader>6", "<Cmd>BufferGoto 6<CR>")
+    map("n", "<Leader>7", "<Cmd>BufferGoto 7<CR>")
+    map("n", "<Leader>8", "<Cmd>BufferGoto 8<CR>")
+    map("n", "<Leader>9", "<Cmd>BufferGoto 9<CR>")
+    map("n", "<Leader>0", "<Cmd>BufferLast<CR>")
     -- Magic buffer-picking mode
     map("n", "<C-p>", "<Cmd>BufferPick<CR>")
+    -- close buffer
+    map("n", "<Leader>w", ":BufferClose<CR>")
   end, -- barbar
 
   gitsigns = function()
