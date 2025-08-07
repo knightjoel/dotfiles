04f60c628f743631ac46c02edb6cae594e1f6e82
Joel Knight <knight.joel@gmail.com>
2025-08-07 15:05:38 -0600

    fix(nvim): Low contrast when viewing diff files

--- .config/nvim/lua/user/core/colors.lua
+++ .config/nvim/lua/user/core/colors.lua
@@ -1,1 +1,17 @@
+-- Override the colors that nightfly chooses for showing diffs.
+-- In particular, the default 'DiffDelete' color sets a bg that has very low
+-- contrast with the default fg color making it all but impossible to read the
+-- text.
+local custom_highlight = vim.api.nvim_create_augroup("CustomNightflyDiff", {})
+vim.api.nvim_create_autocmd("ColorScheme", {
+  pattern = "nightfly",
+  callback = function()
+    local palette = require("nightfly").palette
+    vim.api.nvim_set_hl(0, "DiffAdd", { fg = palette.green })
+    -- You can also improve other diff highlights:
+    vim.api.nvim_set_hl(0, "DiffDelete", { fg = palette.watermelon })
+    vim.api.nvim_set_hl(0, "DiffChange", { fg = palette.regal_blue })
+  end,
+  group = custom_highlight,
+})
 vim.cmd("colorscheme nightfly")
